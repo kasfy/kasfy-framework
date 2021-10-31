dbe01e45f2f58c4a2ef45c2ab6ff51795e48cbd3
katheesh <katheeskumar@outlook.com>
2021-10-31 20:48:09 +0530

    root file  changed

--- src/Server.php
+++ src/Server.php
@@ -4,9 +4,9 @@ namespace Kasfy\Framework;
 
 class Server
 {
-    public function serve()
+    public function serve($root)
     {
-        return shell_exec("php -S localhost:4107 kasfy");
+        return shell_exec("php -S localhost:4107 ". $root);
     }
 }
 
